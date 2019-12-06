c2a784c6cc234aec639525663581e497128436fd
Mohammad Moien <mnmoienian@gmail.com>
2019-12-06 11:54:12 +0330

    define board and cell

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+function Cell(row, column, opened, flagged, mined, neighborMinedCount) {
+  return {
+    id: row + "" + column,
+    row,
+    column,
+    opened,
+    flagged,
+    mined,
+    neighborMinedCount
+  };
+}
 
+function Board(boardSize) {
+  board = {};
+  for (let row = 0; row < boardSize; row++) {
+    for (let column = 0; column < boardSize; column++) {
+      board[row + "" + column] = Cell(row, column, false, false, false, 0);
+    }
+  }
+  // return board;
+  console.log(board);
+}
+
+Board(2);
